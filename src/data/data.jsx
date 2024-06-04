cb2da81c433a2683a1ea4fbb7db61d599de84fe3
Gizzhy <gizzhyakinz@gmail.com>
2024-06-04 13:53:22 +0100

    faqs data added

---
+++ src/data/data.jsx
@@ -0,0 +1,32 @@
+const faq = [
+  {
+    id: 0,
+    question: "How can I withdraw funds from my wallet?",
+    answer:
+      "Due to our policies, direct withdrawal from your wallet is not possible. To withdraw funds, please send an email to customerservice@hospitalsconnect.co with the following details:\nAmount to withdraw\nWallet number\nEmail address\nPhone number\nYour bank account details\nOur team will process your request, and you will receive your withdrawal in 1-3 days.",
+  },
+  {
+    id: 1,
+    question: "Haven't received my order",
+    answer:
+      "If you haven’t received your order after the specified delivery timeframe, please send an email to customerservice@hospitalsconnect.co, specifying the issue, what was purchased, your name, email address or simply place a call to 09017085343. This will aid faster tracking.",
+  },
+  {
+    id: 2,
+    question: "Pending results at a facility.",
+    answer:
+      "If you haven’t received your result at a provided facility beyond the due date, please send an email to customerservice@hospitalsconnect.co, specifying the issue, your name, email address, the facility’s name, and the due date assigned for the result, so we can do a quick follow up.",
+  },
+  {
+    id: 3,
+    question: "Issue(s) with the delivery or item purchased",
+    answer:
+      "If you have an issue with an item you purchased, such as a wrong item delivered or a broken seal, please call 09017085343 immediately.",
+  },
+  {
+    id: 4,
+    question: "I didn't get a cashback",
+    answer:
+      "After successfully paying a facility on the HC app, it typically takes 12-24 hours for the cashback to reflect in your account/wallet. If, after this timeframe, you still haven’t received the cashback, please send an email to customerservice@hospitalsconnect.co. Include details such as the facility, transaction date, a screenshot of the transaction on the HC app, your name, email, and wallet number.",
+  },
+];
